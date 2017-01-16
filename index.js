e4b7d3186654f9f26ede16c72ec1c55746fb8c3b
Boian Katzarsky <katzarsky@gmail.com>
2017-01-16 20:12:18 +0200

    app

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+var express = require('express');
+var app = express();
+
+// Binding express app to port 3000
+app.listen(3000,function(){
+    console.log('Node server running @ http://localhost:3000')
+});
 
